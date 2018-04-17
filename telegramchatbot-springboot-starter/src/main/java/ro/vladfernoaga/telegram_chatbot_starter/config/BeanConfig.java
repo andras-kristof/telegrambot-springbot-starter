8ec92126022906a9af4b33377bea04b0956e5785
vlad <fernoaga.vlad@gmail.com>
2018-04-17 21:26:40 +0300

    feat(): add inline keybord

--- telegramchatbot-springboot-starter/src/main/java/ro/vladfernoaga/telegram_chatbot_starter/config/BeanConfig.java
+++ telegramchatbot-springboot-starter/src/main/java/ro/vladfernoaga/telegram_chatbot_starter/config/BeanConfig.java
@@ -19,7 +19,7 @@ public class BeanConfig {
 	/** The Constant LOGGER. */
 	public static final Logger LOGGER = LogManager.getLogger();
 
-	private static final String API_TOKEN = "<YOUR API TOKEN>";
+	private static final String API_TOKEN = "301393209:AAHmdyFo2w396DQmckL2_nBncXZEqPOXYgU";
 
 	@Autowired
 	private SimpleUpdateHandler updateHandler;
--- telegramchatbot-springboot-starter/src/main/java/ro/vladfernoaga/telegram_chatbot_starter/handler/SimpleUpdateHandler.java
+++ telegramchatbot-springboot-starter/src/main/java/ro/vladfernoaga/telegram_chatbot_starter/handler/SimpleUpdateHandler.java
@@ -11,6 +11,8 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ForceReply;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 
@@ -26,21 +28,36 @@ public class SimpleUpdateHandler implements UpdatesListener {
 	
 	@Override
 	public int process(List<Update> updates) {
-		
+
 		for (Update update : updates) {
 			Integer chatId = update.message().from().id();
 			String messageText = update.message().text();
 			Integer messageId = update.message().messageId();
-			
-			SendMessage request = new SendMessage(chatId, String.format("<b>Hello World</b> I recived your message: %s",messageText))
-					.parseMode(ParseMode.HTML)
-					.disableNotification(false)
-					.replyToMessageId(messageId)
-					.replyMarkup(new ForceReply())
-					;
-			bot.execute(request);
+
+			if (messageText.equals("inline")) {
+				InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+				        new InlineKeyboardButton[]{
+				                new InlineKeyboardButton("button1").callbackData("callback_data"),
+				                new InlineKeyboardButton("button2").callbackData("callback_data"),
+				                new InlineKeyboardButton("Google").callbackData("callback_data").url("wwww.google.ro")
+				        });
+				
+				SendMessage request = new SendMessage(chatId,
+						String.format("<b>Hello inline  %s", messageText))
+								.disableNotification(false).replyToMessageId(messageId)
+								.replyMarkup(inlineKeyboard);
+				bot.execute(request);
+				
+			} else {
+
+				SendMessage request = new SendMessage(chatId,
+						String.format("<b>Hello World</b> I recived your message: %s", messageText))
+								.parseMode(ParseMode.HTML).disableNotification(false).replyToMessageId(messageId)
+								.replyMarkup(new ForceReply());
+				bot.execute(request);
+			}
 		}
-		
+
 		return UpdatesListener.CONFIRMED_UPDATES_ALL;
 	}
 
